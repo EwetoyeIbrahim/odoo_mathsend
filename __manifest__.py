9ba88ee44d9c39880f7327c0f4186022a59c87c9
EwetoyeIbrahim <i.ewetoye@gmail.com>
2022-08-18 12:46:07 +0100

    0.0.1

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Employee Close Point of Sale Session",
+    "version": "0.0.1",
+    "category": "Sales/Point of Sale",
+    'summary': "Allow employees access to close session button",
+    'description': "",
+    "authour": "Ewetoye Ibrahim",
+    "auto_install": False,
+    "depends": ["point_of_sale", "pos_hr"],
+    'installable': True,
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_employee_close_session/Chrome.js',
+            ],
+    },
+    'license': 'LGPL-3',
+}
