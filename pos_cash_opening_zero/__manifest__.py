182e76d921116508b9e98c490375fa58453c1362
Ewetoye Ibrahim <I.ewetoye@gmail.com>
2022-08-25 21:26:35 +0100

    Update banner

--- pos_cash_opening_zero/__manifest__.py
+++ pos_cash_opening_zero/__manifest__.py
@@ -15,6 +15,6 @@
             'pos_cash_opening_zero/static/src/js/Popups/CashOpeningPopup.js',
             ]
     },
-    'images': ['static/description/icon.png', 'static/description/banner_screenshot.JPG'],
+    'images': ['static/description/banner.png'],
     'license': 'LGPL-3',
 }
