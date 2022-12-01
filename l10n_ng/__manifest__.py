171df48c02f89c931861137390e10e158a3ccfed
EwetoyeIbrahim <i.ewetoye@gmail.com>
2022-12-01 23:30:06 +0100

    Lagos State LGs and LCDAs

---
+++ l10n_ng/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Nigeria Localization",
+    "version": "0.0.1",
+    "category": "Localizations",
+    'summary': "Set Nigerian states and lagos state LG and LCDA as cities",
+    "author": "Ewetoye Ibrahim",
+    "website" : "https://github.com/EwetoyeIbrahim/odoo_mathsend",
+    "auto_install": False,
+    "depends": ["base_address_city"],
+    'installable': True,
+    'application': False,
+    'data': [
+        'data/res.country.state.csv',
+        'data/res.city.csv',
+        'views/res_partner_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
