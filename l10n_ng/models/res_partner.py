a09806304f01ee1cc0269e437f6cc804df52e1a4
EwetoyeIbrahim <i.ewetoye@gmail.com>
2022-12-02 23:31:06 +0100

    LCDA Selection closes #2

--- l10n_ng/models/res_partner.py
+++ l10n_ng/models/res_partner.py
@@ -6,10 +6,10 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     city_id = fields.Many2one(
-        'res.city', string='City of Address', domain="[('state_id', '=', state_id)]")
+        'res.city', string='Local Area', domain="[('state_id', '=', state_id)]")
 
     @api.onchange('state_id')
     def _onchange_state(self):
         super(Partner, self)._onchange_state()
-        if self.state_id and self.state_id != self.city_id.state_id:
+        if not(self.state_id) or (self.state_id and self.state_id != self.city_id.state_id):
             self.city_id = False
